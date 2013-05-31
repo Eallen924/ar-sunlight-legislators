620098bc81416864e88ca2f79841b28192d1aae2
Eric Allen <eric.justin.allen@gmail.com>
2013-05-30 22:33:40 -0500

    WIP - need to populate data

--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -1,15 +1,28 @@
 require 'csv'
+require_relative '../app/models/legislator'
 
 class SunlightLegislatorsImporter
   def self.import(filename)
+    congress_critters = []
     csv = CSV.new(File.open(filename), :headers => true)
     csv.each do |row|
-      row.each do |field, value|
-        # TODO: begin
-        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
+      name = " #{row[:firstname]} #{row[:lastname]}"
+      congress_critters << { name:       name,
+                             email:      row[:email],
+                             phone:      row[:phone],
+                             fax:        row[:fax],
+                             website:    row[:website],
+                             webform:    row[:webform],
+                             party:      row[:party],
+                             gender:     row[:gender],
+                             birthdate:  row[:birthdate],
+                             twitter_id: row[:twitter_id],
+                                                          }
+
+        #raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
         # TODO: end
-      end
     end
+    member = Legislator.create!(congress_critters)
   end
 end
 
@@ -21,3 +34,8 @@ rescue ArgumentError => e
 rescue NotImplementedError => e
   $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
 end
+#"title","firstname","middlename","lastname","name_suffix","nickname","party",
+#""state","district","in_office","gender","phone","fax","website","webform",
+#"congress_office","bioguide_id","votesmart_id","fec_id","govtrack_id","crp_id"
+#","twitter_id","congresspedia_url","youtube_url","facebook_id","official_rss"
+#","senate_class","birthdate"
